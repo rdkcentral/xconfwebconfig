5d79522e17dd266f85f2340d2f66673eb28f0924
gravi21 <Gopinath_Ravi@comcast.com>
2026-01-29 07:44:10 +0530

    Fixed few error cases for acnt data

--- dataapi/estb_firmware_context.go
+++ dataapi/estb_firmware_context.go
@@ -298,7 +298,7 @@ func AddEstbFirmwareContext(ws *xhttp.XconfServer, r *http.Request, contextMap m
 	}
 
 	if Xc.EnableAccountService && util.IsUnknownValue(contextMap[common.PARTNER_ID]) {
-		log.WithFields(fields).Errorf("Fallback Trying via Old Account Service,Failed to Get AccountId via Grp Service for MAC='%s' due to Flag Disabled or err ,Err: %v", macAddress, err)
+		log.WithFields(fields).Warnf("Fallback Trying via Old Account Service,Failed to Get AccountId via Grp Service for MAC='%s' due to Flag Disabled or err ,Err: %v", macAddress, err)
 		partnerId := GetPartnerFromAccountServiceByHostMac(ws, contextMap[common.ESTB_MAC], satToken, fields)
 		if partnerId != "" {
 			contextMap[common.PARTNER_ID] = partnerId
